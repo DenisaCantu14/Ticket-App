1d936622a4c80bcb473e753968cf9279680727d1
DenisaCantu14 <62349123+DenisaCantu14@users.noreply.github.com>
2021-04-27 20:47:53 +0300

    added routing

--- react-ticketapp/src/App.js
+++ react-ticketapp/src/App.js
@@ -1,10 +1,44 @@
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import Nav from "./components/Nav";
+import Auth from "./auth/Auth";
+import Callback from "./components/Callback";
 
-import './App.css';
-
-function App() {
-  return (
-   <></>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.auth = new Auth(this.props.history);
+  }
+  render() {
+    return (
+      <>
+        <Nav auth={this.auth} />
+        <div className="body">
+          <Route
+            path="/"
+            exact
+            render={props => <Home auth={this.auth} {...props} />}
+          />
+          <Route
+            path="/callback"
+            render={props => <Callback auth={this.auth} {...props} />}
+          />
+          <Route
+            path="/profile"
+            render={props =>
+              this.auth.isAuthenticated() ? (
+                <Profile auth={this.auth} {...props} />
+              ) : (
+                <Redirect to="/" />
+              )
+            }
+          />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
--- react-ticketapp/src/index.js
+++ react-ticketapp/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Route component={App} />
+  </Router>,
+  document.getElementById("root")
 );
 
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
