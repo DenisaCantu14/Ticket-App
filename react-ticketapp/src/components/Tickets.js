03880176d1f38fae7793cf5e7a836faad425761e
DenisaCantu14 <62349123+DenisaCantu14@users.noreply.github.com>
2021-04-28 22:21:43 +0300

    display info

--- react-ticketapp/src/components/Tickets.js
+++ react-ticketapp/src/components/Tickets.js
@@ -1,72 +1,115 @@
 import React, { useState, useEffect } from "react";
-import Table from 'react-bootstrap/Table'
+import Table from "react-bootstrap/Table";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTicketAlt } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import "../CSS/Tickets.css";
 
 function Tickets(props) {
   const [tickets, setTickets] = useState([]);
   const [filteredTickets, setFilteredTickets] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const search = props.location.state.search;
+  console.log(search);
+
   useEffect(() => {
     async function fetchData() {
       const endpoint = `https://60882486a6f4a30017425cb9.mockapi.io/tickets/ticket`;
       await (await fetch(endpoint)).json().then((data) => {
-        let sort = data.sort((a,b) => (a.date > b.date) ? 1  : 
-        ((b.date > a.date) ? -1 : 0))
+        let sort = data.sort((a, b) =>
+          a.startDate > b.startDate ? 1 : b.startDate > a.startDate ? -1 : 0
+        );
         setTickets(sort);
+        setLoading(false);
       });
     }
     fetchData();
   }, []);
 
   useEffect(() => {
-    const search = props.location.state.search;
-
     let _tickets = tickets.filter(
       (ticket) =>
         ticket.startLocation === search.startLocation &&
         ticket.destination === search.destination &&
-        new Date(ticket.date).getMonth() === search.date.getMonth() &&
-        new Date(ticket.date).getDay() === search.date.getDay() + 1 &&
-        new Date(ticket.date).getYear() === search.date.getYear()
+        new Date(ticket.startDate).getMonth() === search.date.getMonth() &&
+        new Date(ticket.startDate).getDay() === search.date.getDay() + 1 &&
+        new Date(ticket.startDate).getYear() === search.date.getYear()
     );
     setFilteredTickets(_tickets);
   }, [props.location, tickets]);
 
+  function formatDate(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    hours = hours < 10 ? "0" + hours : hours;
+    var strTime = hours + " : " + minutes;
+    return strTime;
+  }
   return (
     <>
-      <h1>Tickets</h1>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>From</th>
-            <th>To</th>
-            <th>Price</th>
-            <th>Places</th>
-            <th>Date</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredTickets.map((ticket) => {
-              let date = ticket.date.toString().replace("T", " ").slice(0, -8);
-            return (
-                <tr key={ticket.id}>
-                  <td>
-                   {ticket.startLocation}
-                  </td>
-                  <td>{ticket.destination}</td>
-                  <td>{ticket.price} RON</td>
-                  <td>{ticket.number}</td>
-                  <td>{date}</td>
-                  <td>
-                    <button
-                      className="btn btn-outline-primary"
-                     
-                    >Buy</button>
-                  </td>
-                </tr>
-              );
-          })}
-        </tbody>
-      </Table>
+      {loading ? (
+        <div>Loading </div>
+      ) : (
+        <>
+          <h4 className="general-info">
+            From {search.startLocation} to {search.destination} -{" "}
+            {search.date.getDate() +
+              "." +
+              (search.date.getMonth() + 1) +
+              "." +
+              search.date.getFullYear()}
+          </h4>
+          <div className="tickets-table">
+          <Table bordered hover striped>
+            <thead>
+              <tr>
+                <th>From</th>
+                <th>To</th>
+                <th>Price</th>
+                <th>Places</th>
+                <th>departure</th>
+                <th>arrival</th>
+                <th>
+                  {" "}
+                  <FontAwesomeIcon icon={faTicketAlt} />
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {tickets.map((ticket) => {
+                let startDate = formatDate(new Date(ticket.startDate));
+
+                let endDate = formatDate(new Date(ticket.endDate));
+                return (
+                  <tr key={ticket.id}>
+                    <td>{ticket.startLocation}</td>
+                    <td>{ticket.destination}</td>
+                    <td>{ticket.price} RON</td>
+                    <td>{ticket.number}</td>
+                    <td>{startDate}</td>
+                    <td>{endDate}</td>
+                    <td>
+                      {ticket.number === 0 ? (
+                        <div className="div-disabled">
+                          <FontAwesomeIcon
+                            icon={faShoppingCart}
+                          ></FontAwesomeIcon>
+                          <div className="div-no-online-line"></div>
+                        </div>
+                      ) : (
+                        <button className="btn btn-primary ">Buy</button>
+                      )}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+          </div>
+        </>
+      )}
     </>
   );
 }
